c710814ffeefc849cf88200a493ea3de62b5ffd3
Ohta Akira <waterlow3442@gmail.com>
2020-12-16 01:30:26 +0900

    コンポーネント分割&FC化

---
+++ src/board.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react';
+import Square from './square';
+
+const Board : React.FC = () => {
+  const status = 'Next player: X';
+  const renderSquare = useCallback((i) => {
+    return <Square />
+  }, [])
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+export default Board
---
+++ src/square.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square: React.FC = () => (
+  <button className="square"></button>
+)
+
+export default Square
